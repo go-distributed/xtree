3140b374ecfd3ae54127320a446d36b7fab70767
Yutong Pei <yutong@upthere.com>
2014-09-28 16:11:06 -0700

    add record reader. fail on test

--- db/record/reader.go
+++ db/record/reader.go
@@ -1,14 +1,50 @@
 package record
 
-import "io"
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"hash/crc32"
+	"io"
+)
+
+const ()
+
+var (
+	crcTable = crc32.MakeTable(crc32.Koopman)
+)
 
 type Reader struct {
+	ra io.ReaderAt
 }
 
 func NewReader(r io.ReaderAt) *Reader {
-
+	return &Reader{ra: r}
 }
 
-func (r *Reader) ReadAt(index int64) (*io.Reader, error) {
+func (r *Reader) ReadAt(index int64) (io.Reader, error) {
+	//TODO change 4 to const
+	crcSlice := make([]byte, 4, 4)
+	valLenSlice := make([]byte, 4, 4)
+	if _, err := r.ra.ReadAt(crcSlice, index); err != nil {
+		return nil, err
+	}
 
+	if _, err := r.ra.ReadAt(valLenSlice, index+4); err != nil {
+		return nil, err
+	}
+
+	crc := binary.LittleEndian.Uint32(crcSlice)
+	valLen := binary.LittleEndian.Uint32(valLenSlice)
+
+	value := make([]byte, valLen, valLen)
+	if _, err := r.ra.ReadAt(value, index+4+4); err != nil {
+		return nil, err
+	}
+
+	if crc != crc32.Update(crc32.Checksum(valLenSlice, crcTable), crcTable, value) {
+		return nil, errors.New("crc unmatch, data corrupted")
+	}
+
+	return bytes.NewBuffer(value), nil
 }
--- db/record/record_test.go
+++ db/record/record_test.go
@@ -1,1 +1,58 @@
 package record
+
+import (
+	"bytes"
+	"encoding/binary"
+	"hash/crc32"
+	"io"
+	"io/ioutil"
+	"testing"
+)
+
+func TestSimpleRead(t *testing.T) {
+
+	testEq := func(a, b []byte) bool {
+		if len(a) != len(b) {
+			return false
+		}
+		for i := range a {
+			if a[i] != b[i] {
+				return false
+			}
+		}
+		return true
+	}
+
+	var buf bytes.Buffer
+	var err error
+	var valReader io.Reader
+	var readValue []byte
+	realValue := []byte("Hello, World")
+	realLen := len(realValue)
+	valLenSlice := make([]byte, 4, 4)
+	crcSlice := make([]byte, 4, 4)
+	binary.LittleEndian.PutUint32(make([]byte, 4, 4), uint32(realLen))
+	crc := crc32.Update(crc32.Checksum(valLenSlice, crcTable), crcTable, realValue)
+	binary.LittleEndian.PutUint32(crcSlice, crc)
+
+	buf.Write(make([]byte, 9, 9))
+	buf.Write(crcSlice)
+	buf.Write(valLenSlice)
+	buf.Write(realValue)
+
+	r := NewReader(bytes.NewReader(buf.Bytes()))
+	valReader, err = r.ReadAt(9)
+	if err != nil {
+		t.Errorf("error")
+	}
+
+	readValue, err = ioutil.ReadAll(valReader)
+	if err != nil {
+		t.Errorf("error")
+	}
+
+	if ok := testEq(realValue, readValue); ok != true {
+		t.Errorf("error")
+	}
+
+}

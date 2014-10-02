b2448de0296fd05efb0e0a433f19ce473f0a0628
Hongchao Deng <hdeng@cloudera.com>
2014-10-02 09:09:54 -0700

    clean up resources in test; nit in io_test

--- db/recordio/io_test.go
+++ db/recordio/io_test.go
@@ -8,37 +8,47 @@ import (
 )
 
 func TestFetch(t *testing.T) {
-	writeFile, err := ioutil.TempFile("", "backend")
+	writeFile, err := ioutil.TempFile("", "testfetch")
 	if err != nil {
 		t.Error("can't create temp file")
 	}
+	defer os.Remove(writeFile.Name())
+	defer writeFile.Close()
+
 	readFile, err := os.Open(writeFile.Name())
 	if err != nil {
 		t.Error("can't open temp file")
 	}
+	defer readFile.Close()
 
 	ap := NewAppender(writeFile)
 	fc := NewFetcher(readFile)
 
-	tests := []Record{
-		{[]byte("someData")},
-		{[]byte("someOtherData")},
+	tests := []struct {
+		offset int64
+		record Record
+	}{
+		{-1, Record{[]byte("someData")}},
+		{-1, Record{[]byte("someOtherData")}},
 	}
 
-	for i, recWrite := range tests {
-		offset, err := ap.Append(recWrite)
+	for i, tt := range tests {
+		offset, err := ap.Append(tt.record)
 		if err != nil {
 			t.Errorf("#%d: Append failed: %s", i, err.Error())
 		}
+		tests[i].offset = offset
+	}
 
-		recRead, err := fc.Fetch(offset)
+	for i, tt := range tests {
+		recRead, err := fc.Fetch(tt.offset)
 		if err != nil {
 			t.Errorf("#%d: Fetch failed: %s", i, err.Error())
 		}
 
-		if !reflect.DeepEqual(recRead, recWrite) {
+		if !reflect.DeepEqual(recRead, tt.record) {
 			t.Errorf("#%d: records not the same, want: %v, get %v",
-				i, recWrite, recRead)
+				i, tt.offset, recRead)
 		}
 
 	}
